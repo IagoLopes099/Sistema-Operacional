7c33310b67b75794e88f2a9f8fa1efad20217c05
vboxuser <iagovitorcz@gmail.com>
2026-06-28 21:32:32 +0000

    Hello World OS

--- kmain.c
+++ kmain.c
@@ -1,6 +1,17 @@
 #include "fb.h"
 
-void kmain(void) {
+
+void clear_screen() {
+    char *fb = (char *)0xB8000;
+
+    for (int i = 0; i < 80 * 25; i++) {
+        fb[i * 2] = ' ';
+        fb[i * 2 + 1] = 0x0F;
+    }
+}
+
+void kmain() {
+    clear_screen();
     char *msg = "Hello, OS World!";
     fb_write(msg, 16);
 }
